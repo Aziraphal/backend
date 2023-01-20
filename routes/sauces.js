350928afa6e4855ad48f3dd1508fe710e7051e15
Cyril Paquier <cyril.paquier@gmail.com>
2023-01-20 12:29:09 +0100

    token ajouté

--- routes/sauces.js
+++ routes/sauces.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 const router = express.Router();
+
 const saucesCtrl = require('../controllers/sauces');
-router.get('/', saucesCtrl.getAllSauces);
-router.post('/', saucesCtrl.createThing);
-router.get('/:id', saucesCtrl.getOneThing);
-router.put('/:id', saucesCtrl.modifyThing);
-router.delete('/:id', saucesCtrl.deleteThing);
+router.get('/', auth, saucesCtrl.getAllSauces);
+router.post('/', auth, saucesCtrl.createThing);
+router.get('/:id', auth, saucesCtrl.getOneThing);
+router.put('/:id', auth, saucesCtrl.modifyThing);
+router.delete('/:id', auth, saucesCtrl.deleteThing);
 
 module.exports = router;
