81dc4709e8c31926571fb32dacd737ac7918572d
Cyril Paquier <cyril.paquier@gmail.com>
2023-01-25 18:19:33 +0100

    correction mime types

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,5 +1,6 @@
 const multer = require('multer');
-const MIME_TYPE = {
+
+const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
     'image/png': 'png'
@@ -10,9 +11,9 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, callback) =>{
         const name = file.originalname.split(' ').join('_');
-        const extension = MIME_TYPE[file.mimetype];
+        const extension = MIME_TYPES[file.mimetype];
         callback(null, name + Date.now() + '.' + extension);
     }
 })
 
-module.exports = multer({ storage }).single('image');
+module.exports = multer({storage: storage }).single('image');
