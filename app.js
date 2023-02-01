72d1aba76717281d24cca894c27117b850a68096
Cyril Paquier <cyril.paquier@gmail.com>
2023-02-01 18:07:31 +0100

    création d'un .env pour exemple

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const helmet = require("helmet");
 const path = require('path');
 const mongoose = require('mongoose');
 const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 const app = express();
 
 dotenv.config();
@@ -16,14 +15,13 @@ mongoose.connect( process.env.SECRET_DB,
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res, next) => {
+  app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
 
-app.use(helmet());
 app.use(express.json());
 
 app.use('/api/sauces', saucesRoutes);
